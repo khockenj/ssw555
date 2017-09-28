ebc7f08349c8fe09b009b2095d3dfce1bd3597dc
khockenj <slyster1010@gmail.com>
2017-09-27 20:27:34 -0400

    Merged user stories
    150 + date after today

--- methods.py
+++ methods.py
@@ -1,3 +1,14 @@
 #So, put all of your methods in this file, and if you want to call them in the gedcom2csv call it as meths.METHODNAME
+from datetime import datetime
+
+def afterCurrentDate(aDate):
+	if aDate > datetime.today().date():
+		return True #invalid date
+	else:
+		return False
+		
 def days_difference(d1, d2):
-	return (d2-d1).days
+	if afterCurrentDate(d1): 
+		return 0
+	else:
+		return (d2-d1).days
