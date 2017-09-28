520fb734c2eaed626a0080a0ddad2bdecd96c424
khockenj <slyster1010@gmail.com>
2017-09-28 04:14:03 -0400

    Just some updates on divorced/married dates
    Also, made the open file open to a variable name so we can change them all at once instead of individually changing them all. All divorce/marriage dates in family lists should be handled correctly.

--- gedcom2csv.py
+++ gedcom2csv.py
@@ -2,13 +2,14 @@
 import csv
 from datetime import datetime, date
 import methods as meths
+openedFile = "testGED.ged" #This will change the open file for ALL (families/indi) - it's cleaner this way and we won't forget to change all of them now
 
-with open('testGED.ged', 'r') as in_file:
+with open(openedFile, 'r') as in_file:
 	with open('individuals.csv', 'w', newline='') as out_file:
 		writer = csv.writer(out_file)
 		counter = 0
 		#Defaults for GEDCOMS that are missing things
-		name = "Name"
+		name = "Unk"
 		sex = "Sex"
 		birth = "Birth"
 		death = "Alive"
@@ -21,18 +22,18 @@ with open('testGED.ged', 'r') as in_file:
 			
 			if lineS[0].strip() == '1' and lineS[1].strip() not in ['SOUR', 'FILE', 'DEST', 'GEDC', 'SUBM', 'SUBN', 'CHAR']:
 				if lineS[1].strip() == 'NAME':
-					name = lineS[2].strip() + " " + lineS[3].strip()
+					name = " ".join(lineS[2:]).strip()
 				elif lineS[1].strip() == 'SEX':
 					sex = lineS[2].strip()
 				elif lineS[1].strip() == 'BIRT':
-					birth = " ".join(next(in_file).split(" ")[2:5]).strip()
+					birth = " ".join(next(in_file).split(" ")[2:]).strip()
 					birthD = datetime.strptime(birth, '%d %b %Y').date()
 					today = datetime.today().date()
 					age = int((meths.days_difference(birthD, today))/365)
 					if(age > 150 or age < 0):
 						age = "INVALID AGE"
 				elif lineS[1].strip() == 'DEAT':
-					death = " ".join(next(in_file).split(" ")[2:5]).strip()
+					death = " ".join(next(in_file).split(" ")[2:]).strip()
 				elif lineS[1].strip() == 'FAMS':
 					fams = lineS[2].strip()
 				elif lineS[1].strip() == 'FAMC':
@@ -45,31 +46,45 @@ with open('testGED.ged', 'r') as in_file:
 					id = lineS[1].strip()
 		writer.writerow((id,name,sex,birth,death,age,famc,fams))
 in_file.close()
-with open('projectGED.ged', 'r') as in_file:	
+with open(openedFile, 'r') as in_file:	
 	with open('families.csv', 'w', newline='') as out_file:
 		writer = csv.writer(out_file)
 		writer.writerow(('FID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children'))
 		child = []
 		divorce = 'Years not provided'
 		married = 'Years not provided'
+		fid = "0"
+		hid = "0"
+		hname = "Unknown"
+		wid = "0"
+		wname = "Unknown"
+
 		counter = 0
 		for line in in_file:
 			lineS = line.split(" ")
-			if lineS[0].strip() == '1' and lineS[1].strip() in ['HUSB', 'CHIL', 'WIFE']:
-				if lineS[1].strip() == 'HUSB':
-					husb = lineS[2].strip()
-				elif lineS[1].strip() == 'WIFE':
-					wife = lineS[2].strip()
-				elif lineS[1].strip() == 'CHIL':
-					child.append(lineS[2].strip())
+			if lineS[0].strip() == '1':
+				if lineS[1].strip() in ['HUSB', 'CHIL', 'WIFE']:
+					if lineS[1].strip() == 'HUSB':
+						husb = lineS[2].strip()
+					elif lineS[1].strip() == 'WIFE':
+						wife = lineS[2].strip()
+					elif lineS[1].strip() == 'CHIL':
+						child.append(lineS[2].strip())
+
+				elif lineS[1].strip() in ['MARR', 'DIV']:
+					if lineS[1].strip() == 'MARR':
+						married = " ".join(next(in_file).split(" ")[2:]).strip()
+					elif lineS[1].strip() == 'DIV':
+						divorce = " ".join(next(in_file).split(" ")[2:]).strip()
+						
 			elif lineS[0].strip() == '0' and lineS[1].strip() not in ['NOTE', 'HEAD', 'TRLR']:
 				if lineS[2].strip() == 'FAM' and counter != 0:
-					with open('projectGED.ged', 'r') as in_file2:
+					with open(openedFile, 'r') as in_file2:
 						for line2 in in_file2:
 							if husb in line2.split(" "):
-								hname = " ".join(next(in_file2).split(" ")[2:4]).strip()
+								hname = " ".join(next(in_file2).split(" ")[2:]).strip()
 							elif wife in line2.split(" "):
-								wname = " ".join(next(in_file2).split(" ")[2:4]).strip()
+								wname = " ".join(next(in_file2).split(" ")[2:]).strip()
 					in_file2.close()
 					writer.writerow((fid,married,divorce,husb,hname,wife,wname," ".join(child)))
 				if lineS[2].strip() != 'INDI':
