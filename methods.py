11d613d3a7455e6e5dfe0fd06c8a4b55b0506b9c
vardaantyagi <vardaan.tyagi@gmail.com>
2017-10-04 23:49:06 -0400

    Update methods.py

--- methods.py
+++ methods.py
@@ -114,3 +114,18 @@ def US_08():
                             print('ERROR: INDIVIDUAL: US08: ' + l +
                               ': Child Birth ' + bday + ' before parents ' + k +
                               ' marriage ' + mardate)
+
+
+def marriage_before_divorce():
+    with open("families.csv", "r+") as fp:
+        for line in fp.readlines():
+            lineS = line.split(",")
+            #print(lineS[2])
+
+            if lineS[1] != "Married" and lineS[2] != "Divorced":
+                bday = (datetime.strptime(lineS[1], '%d %b %Y'))
+                dday = datetime.strptime(lineS[2], '%d %b %Y')
+                if bday > dday:
+                    print('ERROR: INDIVIDUAL: US04: ' + lineS[0] + ': Divorce ' + lineS[2] + ' Marriage ' + lineS[1])
+
+
