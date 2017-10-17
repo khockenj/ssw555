438cd8025c35133cfe8f8db83b51db1269881af6
khockenj <slyster1010@gmail.com>
2017-10-17 12:54:51 -0400

    methods update?

--- main.py
+++ main.py
@@ -12,13 +12,8 @@ readTable = open("families.csv", "r")
 table = from_csv(readTable)
 readTable.close()
 print(table)
-errors = meths.INDI_ONLY()
-for err in errors:
-	print(err)
-errors2 = meths.INDI_FAM_relations()
-for err in errors2:
-	print(err)
-	
+meths.INDI_ONLY()
+meths.INDI_FAM_relations()
 meths.US10()
 meths.US14()
 meths.US28()
--- methods.py
+++ methods.py
@@ -22,11 +22,6 @@ def days_difference(d1, d2, type):
 		typeDict = {'years': 365, 'weeks': 7, 'months': 30.4, 'days': 1}
 		return str(((d2-d1)/typeDict[type]).days)
 
-def test_US27():
-	birthday = datetime.datetime(1900,5,10).date()
-	assert int(days_difference(birthday,today,'years')) == 117
-#def test_US13():
-
 def INDI_FAM_relations():
 	err = []
 	today = datetime.datetime.today().date()
@@ -50,7 +45,7 @@ def INDI_FAM_relations():
 				file2.readline()
 				birthH = None
 				birthW = None
-
+				
 				for row2 in csv.reader(file2,delimiter=','):
 					if husb in row2:	#Pretty sure alot of the user stories are basic if statements in here then we can make it a more generic method name
 						birthH = row2[3]
@@ -70,8 +65,8 @@ def INDI_FAM_relations():
 					if len(children) > 1:
 						for i in children:
 							if i in row2:
-								childbirth[i] = datetime.datetime.strptime(row2[3], '%d %b %Y').date()
-
+								childbirth[i] = datetime.datetime.strptime(row2[3], '%d %b %Y').date()	
+						
 						temp = childbirth.copy()
 						for key, value in childbirth.items():
 							temp.pop(key)
@@ -89,56 +84,17 @@ def INDI_FAM_relations():
 						if len(children) > 0:
 							for x in children: #x = childID
 								if x in row2:
-									if div > datetime.datetime(1, 1, 1).date():
-										if int(days_difference(div,datetime.datetime.strptime(row2[3], '%d %b %Y').date(), 'months')) > 9:
-											err.append('ERROR: US08: ' + x + "'s birthday(" + str(datetime.datetime.strptime(row2[3], '%d %b %Y').date()) + ") is more than 9 months after their parent's divorce(" + str(div) + ")")
-									if married > datetime.datetime(1, 1, 1).date():
-										if int(days_difference(datetime.datetime.strptime(row2[3], '%d %b %Y').date(), married, 'days')) > 0:
-											err.append('ERROR: US08: ' + x + "'s birthday(" + str(datetime.datetime.strptime(row2[3], '%d %b %Y').date()) + ") is before their parent's marriage(" + str(married) + ")")
-									if deathH != 'Alive':
-										if int(days_difference(deathH, datetime.datetime.strptime(row2[3], '%d %b %Y').date(), 'months')) > 9:
-											err.append('ERROR: US09: ' + x + "'s birthday(" + str(datetime.datetime.strptime(row2[3], '%d %b %Y').date()) + ") is more than 9 months after their father's death(" + str(deathW) + ")")
-									if deathW != 'Alive':
-										if int(days_difference(deathW, datetime.datetime.strptime(row2[3],'%d %b %Y').date(), 'days')) > 0:
-											err.append('ERROR: US09: ' + x + "'s birthday(" + str(datetime.datetime.strptime(row2[3], '%d %b %Y').date()) + ") is after their mothers's death(" + str(deathW) + ")")
+										US0809(div, married, deathH, deathW, x, row2[3])
 				US15(children, husb, wife)
 					#US02/03 Birth after marriage, death before marriage
 				if birthW != None and birthH != None:
 					if married > datetime.datetime(1, 1, 1).date() and isinstance(married, datetime.date):	#to account for date=today for bad marriage dates
-						if birthH > married:
-							err.append("ERROR: US02: " + husb + "'s birth date (" + str(birthH) + ') is after his marriage date (' + str(married) + ')')
-						if birthW > married:
-							err.append("ERROR: US02: " + wife + "'s birth date (" + str(birthW) + ') is after her marriage date (' + str(married) + ')')
-						if deathH != "Alive" and deathH < birthH:
-							err.append("ERROR: US02: " + husb + "'s death(" + str(deathH) + ") is before his birth ( " + str(birthH) + ")")
-						if deathW != "Alive" and deathW < birthW:
-							err.append("ERROR: US02: " + wife + "'s death(" + str(deathW) + ") is before her birth ( " + str(birthW) + ")")
-						if deathW != "Alive":
-							if deathW < married:
-								err.append("ERROR: US05: " + wife + "'s death date(" + str(deathW) + ") is before marriage date(" + str(married) + ")")
-						if deathH != "Alive":
-							if deathH < married:
-								err.append("ERROR: US05: " + husb + "'s death date(" + str(deathH) + ") is before marriage date(" + str(married) + ")")
+						US0205(birthH, birthW, married, deathH, deathW, husb, wife)
 						#US 05/06  Marriage before death and divorce before death
 					if div > datetime.datetime(1, 1, 1).date() and isinstance(div, datetime.date):
-						if deathH != 'Alive':
-							if deathH < div:
-								err.append("ERROR: US06: " + husb + "'s death date(" + str(deathH) + ") is before divorce date(" + str(div) + ")")
-						if deathW != 'Alive':
-							if deathW < div:
-								err.append("ERROR: US06: " + wife + "'s death date(" + str(deathW) + ") is before divorce date(" + str(div) + ")")
+						US06(deathH, deathW, div, husb, wife)
 						#US01/04
-					if div > datetime.datetime(1, 1, 1).date():
-						if afterDate(div,today):
-							err.append("ERROR: US01: " + husb + " and " + wife + "'s divorce date(" + str(div) + ") is after the current date(" + str(today) + ")")
-					if married > datetime.datetime(1, 1, 1).date():
-						if afterDate(married,today):
-							err.append("ERROR: US01: " + husb + " and " + wife + "'s marriage date(" + str(div) + ") is after the current date(" + str(today) + ")")
-					if married > datetime.datetime(1, 1, 1).date() and datetime.datetime(1, 1, 1).date() < div:
-						if afterDate(married, div):
-							err.append("ERROR: US04: " + husb + " and " + wife + "'s divorce date(" + str(div) + ") is before their marriage date(" + str(married) + ")")
-					else:
-						err.append("ERROR: GENERAL: Marriage or Divorce date for " + husb  + " and " + wife + " not available")
+					US0104(div, today, husb, wife, married)
 	return err
 
 def INDI_ONLY():
@@ -151,12 +107,12 @@ def INDI_ONLY():
 			age = int(row[5])
 			if dday != 'Alive':
 				dday = datetime.datetime.strptime(dday, '%d %b %Y').date()
-
+			
 				US01(today, dday, bday, row[0])
-				US03(dday, bday, age, row[0])
+				US03(dday, bday, row[0])
 			US07(age, row[0])
 	return 0
-
+	
 def US01(today, dday, bday, row):
 	if today < dday:
 		print('ERROR: US01: ' + row + "'s death(" + str(dday) + ") is after today(" + str(today) + ")")
@@ -165,16 +121,91 @@ def US01(today, dday, bday, row):
 		print('ERROR: US01: ' + row + "'s birth(" + str(bday) + ") is after today(" + str(today) + ")")
 		return False
 	return True
+def US0104(div, today, husb, wife, married):
+	r = True
+	if div > datetime.datetime(1, 1, 1).date():
+		if afterDate(div,today):
+			r = False
+			print("ERROR: US01: " + husb + " and " + wife + "'s divorce date(" + str(div) + ") is after the current date(" + str(today) + ")")
+	if married > datetime.datetime(1, 1, 1).date():
+		if afterDate(married,today):
+			r = False
+			print("ERROR: US01: " + husb + " and " + wife + "'s marriage date(" + str(div) + ") is after the current date(" + str(today) + ")")
+	if married > datetime.datetime(1, 1, 1).date() and datetime.datetime(1, 1, 1).date() < div:
+		if afterDate(married, div):
+			r = False
+			print("ERROR: US04: " + husb + " and " + wife + "'s divorce date(" + str(div) + ") is before their marriage date(" + str(married) + ")")
+	else:
+		print("ERROR: GENERAL: Marriage or Divorce date for " + husb  + " and " + wife + " not available")
+	return r
+	
+def US0205(birthH, birthW, married, deathH, deathW, husb, wife):
+	r = True
+	if birthH > married:
+		print("ERROR: US02: " + husb + "'s birth date (" + str(birthH) + ') is after his marriage date (' + str(married) + ')')
+		r = False
+	if birthW > married:
+		print("ERROR: US02: " + wife + "'s birth date (" + str(birthW) + ') is after her marriage date (' + str(married) + ')')
+		r = False
+	if deathH != "Alive" and deathH < birthH:
+		print("ERROR: US02: " + husb + "'s death(" + str(deathH) + ") is before his birth ( " + str(birthH) + ")")
+		r = False
+	if deathW != "Alive" and deathW < birthW:
+		print("ERROR: US02: " + wife + "'s death(" + str(deathW) + ") is before her birth ( " + str(birthW) + ")")
+		r = False
+	if deathW != "Alive":
+		if deathW < married:
+			print("ERROR: US05: " + wife + "'s death date(" + str(deathW) + ") is before marriage date(" + str(married) + ")")
+			r = False
+	if deathH != "Alive":
+		if deathH < married:
+			print("ERROR: US05: " + husb + "'s death date(" + str(deathH) + ") is before marriage date(" + str(married) + ")")
+			r = False
+	return r
+	
+def US03(dday, bday, row):
+	if bday > dday:
+		print('ERROR: US03: ' + row + "'s death(" + str(dday) + ') is their before birth(' + str(bday) +')')
+		return False
+	return True
 
-def US03(dday, bday, age, row):
-		if bday > dday:
-			print('ERROR: US03: ' + row + "'s death(" + str(dday) + ') is their before birth(' + str(bday) +')')
+def US06(deathH, deathW, div, husb, wife):
+	r = True
+	if deathH != 'Alive':
+		if deathH < div:
+			print("ERROR: US06: " + husb + "'s death date(" + str(deathH) + ") is before divorce date(" + str(div) + ")")
+			r = False
+	if deathW != 'Alive':
+		if deathW < div:
+			print("ERROR: US06: " + wife + "'s death date(" + str(deathW) + ") is before divorce date(" + str(div) + ")")
+			r = False
+	return r
 def US07(age, row):
 	if age > 150 or age < 0:
 		print('ERROR: US07: ' + row + "'s age(" + str(age) + ") is older than 150 or less than 0.")
 		return False
 	return True
-
+	
+def US0809(div, married, deathH, deathW, x, row):
+	r = True
+	if div > datetime.datetime(1, 1, 1).date():
+		if int(days_difference(div,datetime.datetime.strptime(row, '%d %b %Y').date(), 'months')) > 9:
+			r = False
+			print('ERROR: US08: ' + x + "'s birthday(" + str(datetime.datetime.strptime(row, '%d %b %Y').date()) + ") is more than 9 months after their parent's divorce(" + str(div) + ")")
+	if married > datetime.datetime(1, 1, 1).date():
+		if int(days_difference(datetime.datetime.strptime(row, '%d %b %Y').date(), married, 'days')) > 0:
+			r = False
+			print('ERROR: US08: ' + x + "'s birthday(" + str(datetime.datetime.strptime(row, '%d %b %Y').date()) + ") is before their parent's marriage(" + str(married) + ")")
+	if deathH != 'Alive':
+		if int(days_difference(deathH, datetime.datetime.strptime(row, '%d %b %Y').date(), 'months')) > 9:
+			r = False
+			print('ERROR: US09: ' + x + "'s birthday(" + str(datetime.datetime.strptime(row, '%d %b %Y').date()) + ") is more than 9 months after their father's death(" + str(deathW) + ")")
+	if deathW != 'Alive':
+		if int(days_difference(deathW, datetime.datetime.strptime(row,'%d %b %Y').date(), 'days')) > 0:
+			r = False
+			print('ERROR: US09: ' + x + "'s birthday(" + str(datetime.datetime.strptime(row, '%d %b %Y').date()) + ") is after their mothers's death(" + str(deathW) + ")")
+	return r
+	
 def US10():
     f = open("families.csv", "r")
     fString = f.read()
@@ -207,10 +238,10 @@ def US10():
         if (k[2] != 'Years not provided') & (k[1] != 'Years not provided') :
             mday = (datetime.datetime.strptime(k[2], '%d %b %Y')).date()
             bday = datetime.datetime.strptime(k[1], '%d %b %Y').date()
-            nbday = bday + timedelta(days=14 * 365)
+            nbday = bday + datetime.timedelta(days=14 * 365)
 
             if mday < nbday:
-                print('ERROR: INDIVIDUAL: US10: ' + k[0] + ' Marriage on ' + k[2] + ' which is before 14 years of Birth which is ' + k[1])
+                print('ERROR: INDIVIDUAL: US10: ' + k[0] + ' Marriage on ' + k[2] + ' which is before 14 years of birth which is ' + k[1])
 def US14():
     i = open("individuals.csv", "r")
     iString = i.read()
@@ -230,17 +261,15 @@ def US14():
         print('ERROR: INDIVIDUAL: US14: Multiple Siblings are')
         for i in range(len(people)):
             print(people[i])
-#US15 (Fewer than 15 siblings): There should be fewer than 15 siblings in a family
 def US15(anArray, husb, wife):
 	if(len(anArray) > 14):
 		print("ERROR: US15: {} and {} have more than 15 children".format(husb, wife))
 		return False
 	return True
-
 def US28():
     with open('families.csv','r+') as fp1:
         ret = True
-        i = 1
+        i = 1 
         for line in fp1.readlines():
             if i == 1:
                 i += 1
